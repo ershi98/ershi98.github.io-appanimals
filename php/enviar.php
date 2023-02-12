3ae1fa5e62378d45d0df62f0f1181ccfcd2d2be6
Sebastian Sánchez <122845344+sebsanchez99@users.noreply.github.com>
2023-02-12 17:21:52 -0500

    Cambios en el contact

---
+++ php/enviar.php
@@ -0,0 +1,12 @@
+<?php
+    $nombre = [_$POST("nombre")];
+    $email = [_$POST("email")];
+    $asunto = [_$POST("asunto")];
+    $mensaje = [_$POST("mensaje")];
+    
+    //datos para correo
+    $correo_destino = "sebasssanchez99527@gmail.com";
+    $asunto = "Contacto desde web AdopPet";
+
+    
+?>
