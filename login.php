94b44e9750510aa27c76833fbd6c86702a81c3f9
ÉRSHI <brayancaceres1998@hotmail.com>
2023-02-27 15:08:24 -0500

    login separado del codigo de php
    solo con las funciones necesariass en los apartados de

--- login.php
+++ login.php
@@ -1,75 +1,7 @@
 <?php
 
-//login.php
-
 //Include Configuration File
-require_once ('php/config.php');
-
-$login_button = '';
-
-
-if(isset($_GET["code"]))
-{
-
- $token = $google_client->fetchAccessTokenWithAuthCode($_GET["code"]);
-
-
- if(!isset($token['error']))
- {
- 
-  $google_client->setAccessToken($token['access_token']);
-
- 
-  $_SESSION['access_token'] = $token['access_token'];
-
-
-  $google_service = new Google_Service_Oauth2($google_client);
-
- 
-  $data = $google_service->userinfo->get();
-
- 
-  if(!empty($data['given_name']))
-  {
-   $_SESSION['user_first_name'] = $data['given_name'];
-  }
-
-  if(!empty($data['family_name']))
-  {
-   $_SESSION['user_last_name'] = $data['family_name'];
-  }
-
-  if(!empty($data['email']))
-  {
-   $_SESSION['user_email_address'] = $data['email'];
-  }
-
-  if(!empty($data['gender']))
-  {
-   $_SESSION['user_gender'] = $data['gender'];
-  }
-
-  if(!empty($data['picture']))
-  {
-   $_SESSION['user_image'] = $data['picture'];
-  }
- }
-}
-
-
-if(isset($_SESSION['access_token'])) {
-    header('Location: perfil.php');
-} else {
-    echo $login_button;
-}
-
-if(!empty($_SESSION['user_first_name']) && !empty($_SESSION['user_last_name']) && !empty($_SESSION['user_email_address']) && !empty($_SESSION['access_token'])) {
-    header('Location: perfil.php');
-    exit();
-  }
-
-  $google_client->setPrompt('select_account');
-$auth_url = $google_client->createAuthUrl();
+require_once ('php/config-login-google.php');
 
 ?>
 
